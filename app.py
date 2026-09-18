68b16e6b997cfa51904d459c6be729eb3e076dea
Addwin Alanolikkal <147745866+add-win@users.noreply.github.com>
2026-09-18 10:28:09 +0530

    Enhance electric bill prediction with fan units
    Updated title and added fan unit input for prediction.

--- app.py
+++ app.py
@@ -6,12 +6,18 @@ from sklearn.preprocessing import PolynomialFeatures
 model = joblib.load("Electric_Bill_AC_model.pkl")
 
 poly = PolynomialFeatures(degree=2)
-poly.fit([[1]])
 
-st.title("Electric Bill Prediction based on AC Units")
+st.title("Electric Bill Prediction based on AC and Fan Units")
 
 ac = st.number_input(
     "Enter the AC Units",
+    min_value=0.0,
+    value=1.0,
+    step=1.0
+)
+
+fan = st.number_input(
+    "Enter the Fan Units",
     min_value=0.0,
     value=1.0,
     step=1.0
@@ -21,10 +27,14 @@ if st.button("Predict"):
 
     if ac < 1 or ac > 150:
         st.error("Cannot predict: AC units must be between 1 and 150.")
+        
+    elif fan < 1 or fan > 150:
+        st.error("Cannot predict: Fan units must be between 1 and 150.")
 
     else:
         input_data = pd.DataFrame({
-            "AC_Units": [ac]
+            "AC_Units": [ac],
+            "Fan_Units": [fan]
         })
 
         new_data_poly = poly.transform(input_data)
