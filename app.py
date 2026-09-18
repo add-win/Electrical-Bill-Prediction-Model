3f49bc7b1c265e4f7b9732596560d922081bcb65
Addwin Alanolikkal <147745866+add-win@users.noreply.github.com>
2026-09-18 10:48:27 +0530

    Refactor electric bill prediction model and inputs
    Updated model loading and polynomial feature fitting. Commented out the 'Other Units' input section and related validation.

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import joblib
 import pandas as pd
 from sklearn.preprocessing import PolynomialFeatures
 
-model = joblib.load("Electric_Bill_AC_Fan_Other_model.pkl")
+model = joblib.load("Electric_Bill_AC_Fan_model.pkl")
 
 poly = PolynomialFeatures(degree=2)
-poly.fit([[1, 1, 1]])
+poly.fit([[1, 1]])
 
 st.title("Electric Bill Prediction based on AC and Fan Units")
 
@@ -24,12 +24,13 @@ fan = st.number_input(
     step=1.0
 )
 
-other = st.number_input(
-    "Enter the Other Units",
-    min_value=0.0,
-    value=1.0,
-    step=1.0
-)
+# other = st.number_input(
+#     "Enter the Other Units",
+#     min_value=0.0,
+#     value=1.0,
+#     step=1.0
+# )
+
 if st.button("Predict"):
 
     if ac < 1 or ac > 150:
@@ -38,14 +39,14 @@ if st.button("Predict"):
     elif fan < 1 or fan > 150:
         st.error("Cannot predict: Fan units must be between 1 and 150.")
 
-    elif other < 1 or other > 150:
-        st.error("Cannot predict: Fan units must be between 1 and 150.")
+    # elif other < 1 or other > 150:
+    #     st.error("Cannot predict: Fan units must be between 1 and 150.")
         
     else:
         input_data = pd.DataFrame({
             "AC_Units": [ac],
             "Fan_Units": [fan],
-            "Other_Units": [other]
+            # "Other_Units": [other]
         })
 
         new_data_poly = poly.transform(input_data)
